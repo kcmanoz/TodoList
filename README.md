e32e24172d2c52d64ac5308a03a3d7651ee4f11b
Manoj KC <manoj.kc@tuni.fi>
2023-07-27 06:24:56 +0300

    Added localStorage functionality for tasks

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-Simple Todo application!
+TODO List Application
+This simple web application is a classic "TODO List" application, where users can create a list of tasks, edit them, and delete them as needed. It's built using HTML, CSS, and JavaScript, and leverages localStorage for persisting the tasks data.
+
+Key Features
+Task Creation: Users can type a task into the input box and click the "Add" button to add it to the list. If the input box is empty when the "Add" button is clicked, an alert prompts the user to enter a task.
+
+Task Editing: Each task has an "Edit" button represented by a Edit Icon. When clicked, the task becomes editable, and the user can change the task text directly. Clicking the "Edit" button again saves the changes and updates the task in the list and in local storage.
+
+Task Deletion: Each task has a "Delete" button. When clicked, the task is removed from the list and deleted from local storage.
+
+Persistent Data: The tasks are stored in the browser's local storage, so they persist across sessions. The tasks are loaded from local storage when the page is loaded, and any changes to the tasks (creation, updates, deletion) are saved to local storage immediately.
---
+++ app.js
@@ -0,0 +1,116 @@
+const todoInput = document.querySelector('#todo-input');
+const todoList = document.querySelector('.todo-list');
+const addBtn = document.querySelector('#add-btn');
+
+// Fetch tasks from local storage
+function getTasks() {
+    const tasks = localStorage.getItem('tasks');
+    return tasks ? JSON.parse(tasks) : [];
+}
+
+// Save tasks to local storage
+function saveTasks(tasks) {
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+
+// Delete task
+function deleteTask(task) {
+    const tasks = getTasks();
+    const updatedTasks = tasks.filter(t => t !== task);
+    saveTasks(updatedTasks);
+}
+
+// Update task
+function updateTask(oldTask, newTask) {
+    const tasks = getTasks();
+    const updatedTasks = tasks.map(t => (t === oldTask ? newTask : t));
+    saveTasks(updatedTasks);
+}
+
+// Create new task element
+function createTaskElement(task) {
+    const li = document.createElement('li');
+    const textInput = document.createElement('input');
+    const editDiv = document.createElement('div');
+    const deleteDiv = document.createElement('div');
+    const editIcon = document.createElement('img');
+    const deleteIcon = document.createElement('img');
+
+    textInput.value = task;
+    textInput.readOnly = true;     // By default, set the input field to read-only
+    textInput.classList.add('todo-text');
+
+    editIcon.src = 'edit.png';
+    editIcon.alt = 'Edit';
+
+    // Variable to track if we're currently editing the task
+    let isEditing = false;
+
+    // Add an event listener to the edit button
+    editIcon.addEventListener('click', () => {
+        // If we're not currently editing, make the input field editable
+        if (!isEditing) {
+            textInput.readOnly = false;
+            textInput.focus();
+            editIcon.src = 'save.png';
+            editIcon.alt = 'Save';
+        } else {
+            // If we're currently editing, save the changes and make the input field read-only again
+            const newText = textInput.value.trim();
+            if (newText !== '') {
+                task = newText;
+                updateTask(task, newText);
+            }
+            textInput.readOnly = true;
+            editIcon.src = 'edit.png';
+        }
+
+        // Toggle the editing status
+        isEditing = !isEditing;
+    });
+
+    // Add the edit button to its parent div
+    editDiv.appendChild(editIcon);
+
+    deleteIcon.src = 'delete.png';
+    deleteIcon.alt = 'Delete';
+    deleteIcon.classList.add('delete-icon');
+
+    deleteIcon.addEventListener('click', (event) => {
+        event.target.parentNode.parentNode.remove();
+        deleteTask(task);
+    });
+
+    deleteDiv.appendChild(deleteIcon);
+
+    li.appendChild(textInput);
+    li.appendChild(editDiv);
+    li.appendChild(deleteDiv);
+
+    return li;
+}
+
+// Add new task
+function addTask(task) {
+    const tasks = getTasks();
+    tasks.push(task);
+    saveTasks(tasks);
+    todoList.appendChild(createTaskElement(task));
+}
+
+// Initialize tasks on load
+window.onload = () => {
+    const tasks = getTasks();
+    tasks.forEach(task => todoList.appendChild(createTaskElement(task)));
+}
+
+// Handle add button click
+addBtn.addEventListener('click', () => {
+    const task = todoInput.value.trim();
+    if (task) {
+        addTask(task);
+        todoInput.value = '';
+    } else {
+        alert("Please, write the task to add!");
+    }
+});
